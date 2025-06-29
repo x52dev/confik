18766760b270a09436dfd4b0877de4c039a2f39b
Rob Ede <robjtede@icloud.com>
2025-06-29 12:34:18 +0100

    build: update nixpkgs 25.05 (#250)

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
